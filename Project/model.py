456aa5ed224c7128938db81fb067b41849af47d9
chelseyin <45832339+chelseyin@users.noreply.github.com>
2019-05-16 21:42:37 -0400

    Update model.py

--- Project/model.py
+++ Project/model.py
@@ -98,3 +98,44 @@ lrevaluator = MulticlassClassificationEvaluator(labelCol="label", predictionCol=
 lraccuracy = lrevaluator.evaluate(lrpredictions)
 lr=['LogisticRegression',str(lraccuracy)]
 print(', '.join(lr))
+
+# RandomForest
+
+df = spark.read.format("csv").option("header", "true").load("chicago/data/food-inspections-new.csv")
+
+df=df.na.drop()
+
+df=df.withColumn("Latitude",df["Latitude"].cast(DoubleType()))
+df=df.withColumn("Longitude",df["Longitude"].cast(DoubleType()))
+
+assembler = VectorAssembler(inputCols=["Latitude","Longitude"],outputCol="assemfeatures")
+
+normalizer = Normalizer(inputCol="assemfeatures", outputCol="features", p=1.0)
+
+df_asse = assembler.transform(df)
+df_norm = normalizer.transform(df_asse)
+
+df=df_norm
+
+indexer = StringIndexer(inputCol="Risk", outputCol="label")
+df = indexer.fit(df).transform(df)
+
+labelIndexer = StringIndexer(inputCol="label", outputCol="indexedLabel").fit(df)
+
+featureIndexer = VectorIndexer(inputCol="features", outputCol="indexedFeatures", maxCategories=4).fit(df)
+
+(trainingData, testData) = df.randomSplit([0.5, 0.5]) 
+
+rf = RandomForestClassifier(labelCol="indexedLabel", featuresCol="indexedFeatures", numTrees=10)
+
+labelConverter = IndexToString(inputCol="prediction", outputCol="predictedLabel",labels=labelIndexer.labels)
+
+rfpipeline = Pipeline(stages=[labelIndexer, featureIndexer, rf, labelConverter])
+
+rfmodel = rfpipeline.fit(trainingData)
+rfpredictions = rfmodel.transform(testData)
+rfpredictions.select("predictedLabel", "label", "features").show(5)
+rfevaluator = MulticlassClassificationEvaluator(labelCol="indexedLabel", predictionCol="prediction", metricName="accuracy")
+rfaccuracy = rfevaluator.evaluate(rfpredictions)
+rf=['RandomForest',str(rfaccuracy)]
+print(', '.join(rf))
